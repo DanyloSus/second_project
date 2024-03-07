b45a5b8fe7512fbc85f3ea33ec04139fbe15f5e9
danylosus <danil.sushko19@gmail.com>
2024-03-07 18:22:46 +0200

    Appearance of the App

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,22 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Text("Sus"),
-    ),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: [],
+        ),
+      ),
+    );
+  }
 }
